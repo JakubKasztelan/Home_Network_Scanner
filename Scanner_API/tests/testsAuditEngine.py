fc6feefd34f37ed61bd47a95f1589d7057350203
Kuba <kubakasztelan05@gmail.com>
2026-05-04 17:47:14 +0200

    Added testsAuditEngine.py test_port_scan_identifies_open_ports()

--- Scanner_API/tests/testsAuditEngine.py
+++ Scanner_API/tests/testsAuditEngine.py
@@ -1,3 +1,5 @@
+import pytest
+
 from services.AuditEngine import AuditEngine
 
 def test_ping_scan_find_hosts():
@@ -12,3 +14,16 @@ def test_ping_scan_find_hosts():
     assert len(found_devices) > 0
 
     assert hasattr(found_devices[0], "ip_address")
+
+def test_port_scan_identifies_open_ports():
+    engine = AuditEngine()
+
+    found_devices = engine.perform_ping_scan()
+
+    if not found_devices:
+        pytest.fail("No devices found to perform port scan test.")
+
+    target_ip = found_devices[0].ip_address
+    open_ports = engine.scan_high_risk_ports(target_ip)
+
+    assert isinstance(open_ports, list)
