4d3f94dcc5997bfe43c6e03db496ed28366841c4
Kuba <kubakasztelan05@gmail.com>
2026-05-04 17:55:30 +0200

    Added testAnalyzer.py test_security_score_calculation()

---
+++ Scanner_API/tests/testsAnalyzer.py
@@ -0,0 +1,14 @@
+import pytest
+
+from models.device import DeviceProfile
+
+
+def test_security_score_calculation():
+    analyzer = SecurityAnalyzer()
+
+    perfect_device = DeviceProfile(ip_address="192.168.1.1", open_ports=[])
+    assert analyzer.calculate_health_score([perfect_device]) == 100
+
+    vulnerable_device = DeviceProfile(ip_address="192.168.1.5", open_ports=[23])
+    score = analyzer.calculate_health_score([vulnerable_device])
+    assert score < 100
