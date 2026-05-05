637fa4f89642ebeeaff95328005a5cf6df900afb
Kuba <kubakasztelan05@gmail.com>
2026-05-05 09:33:13 +0200

    Added CORS support

--- Scanner_API/main.py
+++ Scanner_API/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from api.endpoints import router as audit_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(audit_router, prefix="/api", tags=["Audit"])
 
